cb9f72219f28f36f24ca497a386882d236e22ee7
PriyankaGadela <144148288+PriyankaGadela@users.noreply.github.com>
2023-09-09 12:54:41 +0530

    Add files via upload

---
+++ categoryname.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { AbstractHttpCommunication, MedicineInventory } from '../HttpCommunication';
+
+@Component({
+  selector: 'app-categoryname',
+  templateUrl: './categoryname.component.html',
+  styleUrls: ['./categoryname.component.css']
+})
+
+export class CategorynameComponent {
+  MedicineInventories!: MedicineInventory[];
+  errors!: string;
+  button_clicked: boolean = false;
+  categoryName: string = "";
+  //selectedCategoryName: any;
+  //categoryNames: string[] = []; // Add an array to store category names
+
+  constructor(private service: AbstractHttpCommunication) {}
+
+  categoryByName(): void {
+    
+    let observable = this.service.CategoriesByName(this.categoryName);
+    observable.subscribe({
+      next: (result: any) => {
+        this.MedicineInventories = result;
+        // Extract and store category names from the result
+        //this.categoryNames = result.map((inventory) => inventory.categoryName);
+      },
+      error: (err) => (this.errors = `${err.statusText}  ${err.status}`),
+    });
+    this.button_clicked = true;
+  }
+}
+
